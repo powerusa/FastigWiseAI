aedd534c6ebeafb90781128045f58087e87bab17
Pawel <pawel.palka@yahoo.com>
2025-06-09 17:12:55 -0500

    Update 'BUILT ON BOLT' badge to display in white with increased opacity

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,7 @@ const App: React.FC = () => {
         <div className="mb-6">
           {/* Built on Bolt Badge */}
           <div className="fixed left-4 bottom-28 z-40">
-            <img src={boltBadge} alt="Built on Bolt" className="h-6 opacity-70 dark:invert" />
+            <img src={boltBadge} alt="Built on Bolt" className="h-6 opacity-90" />
           </div>
           
           {/* Bottom tab navigation */}
